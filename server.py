ae9e4a7758207ad1b82db5d11c4a32b8915b44c7
theduylol <trantheduypro@gmail.com>
2025-07-14 17:19:19 +0700

    Update server.py

--- server.py
+++ server.py
@@ -8,3 +8,7 @@ def command():
     global latest_command
     data = request.get_json()
     if data.get("key") != "1234":
+        return "Unauthorized", 403
+    latest_command = data["code"]
+    return "✅ Command received."
+
