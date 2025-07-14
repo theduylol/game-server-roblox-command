4f74f697f86ecb19b25eee27ed8769060dc69683
theduylol <trantheduypro@gmail.com>
2025-07-14 17:04:43 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+latest_command = ""
+
+@app.route("/command", methods=["POST"])
+def command():
+    global latest_command
+    data = request.get_json()
+    if data.get("key") != "1
